4e42bbff2fce97551ce57d0df4bae38813563c87
Tom Hadfield <tomhadfield66@googlemail.com>
2021-11-30 01:01:26 +0000

    changed GGM dir name

---
+++ GGM_scripts/sample_1q8t_GGM.sh
@@ -0,0 +1,8 @@
+python sample.py \
+                --ncpus 1 \
+                --save_fpath "saved_GGM_model.pt" \
+                --output_filename "GGM_elabs.txt" \
+                --item_per_cycle 250 \
+		--scaffold "CC(N)C1CCC(C(N)=O)CC1" \
+                --stochastic
+
